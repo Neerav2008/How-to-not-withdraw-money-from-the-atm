9e678c265089127ad85800a77b40d05c9cce2a12
Neerav2008 <63791548+Neerav2008@users.noreply.github.com>
2021-05-20 19:53:52 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,35 @@
+class ATM(object):
+    def __init__(self, balance, withdrawl_limit, deposit_Limit):
+        self.balance = balance
+        self. withdrawl_Limit = withdrawl_limit
+        self. deposit_limit = deposit_Limit
+
+    def howtonotwithdraw(self):
+        print("How to not widthraw money:")
+        print("go to the farthest atm")
+        print("bring the wrong card")
+        print("go back to car")
+        print("forget the keys")
+        print("let someone steal the keys from the atm and then take the car away")
+        print("walk home")
+        print("drop your wallet")
+        print("just walk back to the house")
+        print("realize you dropped your wallet")
+        print("stay in deppresion for the rest of your life")
+
+
+    def andthendosomethingstupid(self):
+        print("then:")
+        print("go and take all the money from bank account by walking to the farthest bank which is 50km")
+        print("go over the withdrawl limit")
+        print("then attract a lot of attention")
+        print("then try to deposit all that")
+        print("go over the deposit limit")
+        print("get caught for suspicious work")
+        print("get mad and commit a crime of harming a police offer and attempt to murder")
+        print("spoil your life")
+        print("ps: and get your bank acount balance to zero ")
+
+verystupidguy = ATM(0,100 ,50,)
+verystupidguy.howtonotwithdraw()
+verystupidguy .andthendosomethingstupid()
